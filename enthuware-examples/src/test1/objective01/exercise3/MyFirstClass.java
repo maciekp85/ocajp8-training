2e271fa93f22163d96b7b681ac0a79dbca62a87a
Maciej Piotrowski <nishi@wimiip.pl>
2018-01-06 22:49:33 +0100

    [ENT] Exercise 3

---
+++ enthuware-examples/src/test1/objective01/exercise3/MyFirstClass.java
@@ -0,0 +1,9 @@
+package test1.objective01.exercise3;
+
+// javac MyFirstClass.java
+// java MyFirstClass hello hello
+public class MyFirstClass {
+  public static void main(String[] args) {
+    System.out.println(args[1]);
+  }
+}
