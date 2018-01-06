d6fb948129c5f4ef952a5e30895cbc34ae452a57
Maciej Piotrowski <nishi@wimiip.pl>
2018-01-06 20:48:14 +0100

    Created a new module and first exercise

---
+++ vavatech-examples/src/exercise1/GreekTest.java
@@ -0,0 +1,18 @@
+package exercise1;
+
+class Alpha { void m1(){}}
+class Beta extends Alpha { void m2() {}}
+class Gamma extends Beta {}
+
+public class GreekTest {
+
+  public static void main(String args[]) {
+    Alpha[] a = {new Alpha(), new Beta(), new Gamma()};
+    for (Alpha a2 : a) {
+      a2.m1();
+      if (a2 instanceof Beta || a2 instanceof Gamma) {
+        ((Gamma) a2).m2();
+      }
+    }
+  }
+}
