de576eafd68a28405cd48769ec0bb06a2bf5ff1c
Maciej Piotrowski <nishi@wimiip.pl>
2018-01-07 00:06:04 +0100

    [ENT] 02. Working with Java Data Types. Exercise 3

---
+++ enthuware-examples/src/test1/objective02/exercise3/Test3.java
@@ -0,0 +1,11 @@
+package test1.objective02.exercise3;
+
+public class Test3 {
+
+  String s = this.toString();
+
+  public static void main(String[] args) {
+    Test3 t3 = new Test3();
+    System.out.println(t3.s);
+  }
+}
