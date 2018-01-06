36713267f37a6b59dd0ffd1b85b4f2813a221903
Maciej Piotrowski <nishi@wimiip.pl>
2018-01-06 22:13:00 +0100

    Created a new module for examples from enthuware
    [ENT]  Exercise 1

---
+++ enthuware-examples/src/test1/objective01/TestClass.java
@@ -0,0 +1,10 @@
+package test1.objective01;
+
+// Error: Could not find or load main class TestClass
+// It will throw an Error at runtime.
+public class TestClass {
+  public static long main(String[] args) {
+    System.out.println("Hello");
+    return 10L;
+  }
+}
