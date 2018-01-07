d87f9f3ec867d58fb4863bc8ce65bd9ff60b58f7
Maciej Piotrowski <nishi@wimiip.pl>
2018-01-07 18:44:20 +0100

    [ENT] 03. Using operators and decision constructs. Exercise 3

---
+++ enthuware-examples/src/test1/objective03/exercise3/Test3.java
@@ -0,0 +1,9 @@
+package test1.objective03.exercise3;
+
+public class Test3 {
+  public static void main(String[] args) {
+    String str1 = "one";
+    String str2 = "two";
+    System.out.println(str1.equals( str1=str2 ));
+  }
+}
