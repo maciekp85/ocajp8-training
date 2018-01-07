25c744f7a2623c6807860c22a3e5fb32bf7d6311
Maciej Piotrowski <nishi@wimiip.pl>
2018-01-07 19:32:06 +0100

    [ENT] 03. Using operators and decision constructs. Exercise 5

---
+++ enthuware-examples/src/test1/objective03/exercise5/Test5.java
@@ -0,0 +1,8 @@
+package test1.objective03.exercise5;
+
+public class Test5 {
+  public static void main(String[] args) {
+    Object t = new Integer( 107 );
+//    int k = (Integer) t.intValue()/9; // Compiler will complain that the method intValue() is not available in Object.
+  }
+}
