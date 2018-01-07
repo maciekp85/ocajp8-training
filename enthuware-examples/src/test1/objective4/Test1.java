54a6ced901ffcb959f909dc0c427325f0226ef5a
Maciej Piotrowski <nishi@wimiip.pl>
2018-01-08 00:53:01 +0100

    [ENT] 04.Creating and using Arrays. Exercise 1

---
+++ enthuware-examples/src/test1/objective4/Test1.java
@@ -0,0 +1,21 @@
+package test1.objective4;
+
+public class Test1 {
+
+  int[][] matrix = new int[2][3];
+
+  int a[] = {1, 2, 3};
+  int b[] = {4, 5, 6};
+
+  public int compute(int x, int y) {
+    return a[x] * b[y];
+  }
+
+  public void loadMatrix() {
+    for (int x = 0; x < matrix.length; x++) {
+      for (int y = 0; y < matrix[x].length; y++) {
+        matrix[x][y] = compute( x, y );
+      }
+    }
+  }
+}
