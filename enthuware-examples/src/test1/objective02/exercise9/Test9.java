dbd94c82215c90d18d6a5dc24854d16aabeceb9d
Maciej Piotrowski <nishi@wimiip.pl>
2018-01-07 01:18:03 +0100

    [ENT] 02. Working with Java Data Types. Exercise 9

---
+++ enthuware-examples/src/test1/objective02/exercise9/Test9.java
@@ -0,0 +1,10 @@
+package test1.objective02.exercise9;
+
+public class Test9 {
+  public static void main(String[] args) {
+    Integer i = new Integer( 1 );
+    Long m = new Long( 1 );
+    if (i.equals( m )) System.out.println("equal");
+    else System.out.println("not equal");
+  }
+}
