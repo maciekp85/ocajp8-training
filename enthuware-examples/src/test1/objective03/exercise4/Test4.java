09d2857661e732cdddf50f3949b30bf6a4a7011c
Maciej Piotrowski <nishi@wimiip.pl>
2018-01-07 19:00:04 +0100

    [ENT] 03. Using operators and decision constructs. Exercise 4

---
+++ enthuware-examples/src/test1/objective03/exercise4/Test4.java
@@ -0,0 +1,13 @@
+package test1.objective03.exercise4;
+
+public class Test4 {
+  public static void main(String[] args) {
+    boolean b1 = false;
+    boolean b2 = false;
+//    if (b2 != b1 = !b2) {   // Compile time error (b2 != b1 = !b2)
+//      System.out.println("true");
+//    } else {
+//      System.out.println("false");
+//    }
+  }
+}
