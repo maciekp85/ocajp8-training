833158f5cefd7b25a68ead4dafa3ca8b1cc3a987
Maciej Piotrowski <nishi@wimiip.pl>
2018-09-19 23:09:12 +0200

    [VAVA] Exercise 15. Using Duration and Period classes regarding to daylight savings time

---
+++ vavatech-examples/src/exercise15/DaylightSavingsTimeTest.java
@@ -0,0 +1,15 @@
+package exercise15;
+
+import java.time.*;
+
+public class DaylightSavingsTimeTest {
+  public static void main(String[] args) {
+    LocalDateTime ld = LocalDateTime.of( 2015, Month.OCTOBER, 31, 10,0 );
+    ZonedDateTime date = ZonedDateTime.of( ld, ZoneId.of( "US/Eastern" ) );
+    date = date.plus( Duration.ofDays( 1 ) );
+    System.out.println(date);
+    date = ZonedDateTime.of( ld, ZoneId.of( "US/Eastern" ) );
+    date = date.plus( Period.ofDays( 1 ) );
+    System.out.println(date);
+  }
+}
