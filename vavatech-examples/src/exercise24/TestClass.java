ef6bab08efcf1adb6db207d3db271fe1d260e004
Maciej Piotrowski <nishi@wimiip.pl>
2018-09-23 15:30:26 +0200

    [VAVA] Exercise 24. Variables naming

---
+++ vavatech-examples/src/exercise24/TestClass.java
@@ -0,0 +1,11 @@
+package exercise24;
+
+public class TestClass {
+  public static void main(String[] args) {
+//    int 1rstArg = Integer.parseInt( args[1] );  // Compilation fails, wrong variable name, it can not start with digit
+//    System.out.println(args[1rstArg]);
+
+    int rstArg = Integer.parseInt( args[1] );
+    System.out.println(args[rstArg]);
+  }
+}
