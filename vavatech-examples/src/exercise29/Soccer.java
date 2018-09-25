dbdd2a79e4f8130b042646be57f07151fefe80bf
Maciej Piotrowski <nishi@wimiip.pl>
2018-09-25 21:25:53 +0200

    [VAVA] Exercise 29. Example with unhandled exception

---
+++ vavatech-examples/src/exercise29/Soccer.java
@@ -0,0 +1,18 @@
+package exercise29;
+
+class Game {
+  public void play() throws Exception {
+    System.out.println("Playing...");
+  }
+}
+public class Soccer extends Game {
+
+  public void play() {
+    System.out.println("Playing Soccer...");
+  }
+
+  public static void main(String[] args) {
+    Game g = new Soccer();
+//    g.play();  // Compilation fails, unhandled exception
+  }
+}
