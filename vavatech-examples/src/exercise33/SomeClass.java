d816212d002e66f11f5e75492a29b9751226d302
Maciej Piotrowski <nishi@wimiip.pl>
2018-09-27 01:35:46 +0200

    [VAVA] Exercise 33. Constructors

---
+++ vavatech-examples/src/exercise33/SomeClass.java
@@ -0,0 +1,9 @@
+package exercise33;
+
+public class SomeClass {
+  public SomeClass(int i, float f) {}
+  public SomeClass(float f, int i) {}
+  public SomeClass(float f) {}
+  public void SomeClass() {}
+  // No compile errors will be generated
+}
