85fb07fcbe47a769bb831843eb0a265f975b53ee
Maciej Piotrowski <nishi@wimiip.pl>
2018-10-06 09:29:20 +0200

    [VAVA] Exercise 37. while loop example

---
+++ vavatech-examples/src/exercise37/Test1.java
@@ -0,0 +1,10 @@
+package exercise37;
+
+public class Test1 {
+  public static void main(String[] args) {
+    while (true) {}
+    while (true&&(7==7)){}  // Unreachable statement
+    while (7==7) {}
+    while (true);
+  }
+}
