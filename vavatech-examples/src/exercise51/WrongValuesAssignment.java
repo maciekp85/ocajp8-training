1e238e883c90b69e7db6bc3e653294e9fd90e763
Maciej Piotrowski <nishi@wimiip.pl>
2018-10-13 20:00:41 +0200

    [VAVA] Exercise 51. Examples of assigning wrong values to variables

---
+++ vavatech-examples/src/exercise51/WrongValuesAssignment.java
@@ -0,0 +1,11 @@
+package exercise51;
+
+public class WrongValuesAssignment {
+  public static void main(String[] args) {
+//    char d = "d";   // Incompatible types: char -> java.lang.String
+//    float f = 3_000_000_000;   // Integer number too large
+//    int i = 34b;  // cannot resolve symbol 'b'
+//    byte b = 257; // Incompatible types byte -> int
+    Boolean isPresent = true; // correct
+  }
+}
