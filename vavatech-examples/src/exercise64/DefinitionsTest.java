31896b4982521b9864f437179ac007e871800fa7
Maciej Piotrowski <nishi@wimiip.pl>
2018-10-28 21:51:16 +0100

    [VAVA] Exercise 64. Different variations of variables definitions

---
+++ vavatech-examples/src/exercise64/DefinitionsTest.java
@@ -0,0 +1,11 @@
+package exercise64;
+
+public class DefinitionsTest {
+  public static void main(String[] args) {
+//    int x1 = _99; // not correct, can not resolve symbol _99
+//    int x2 = 99_; // not correct, illegal underscore
+    long accountNumber = 0xFF_EC_DE_5E; // correct
+//    short nybbies = 0b0000_0101e1; // not correct, cannot resolve symbol e1
+//    float pi1 = 9_.1415F; // not correct, illegal underscore
+  }
+}
